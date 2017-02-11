3b1323c60a80e44220472cd2f8b69b8d44a9245e
Gary Kertis <gary.kertis@gmail.com>
2017-02-10 21:25:54 -0500

    Add some notes, clean up.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,16 +1,12 @@
 import { Component, OnDestroy, AfterViewInit, EventEmitter, Input, Output, NgZone, ViewEncapsulation, ElementRef, Renderer } from '@angular/core';
 import { DomSanitizer } from '@angular/platform-browser';
 import { CommentService } from '../services/comment.service';
-import * as parse5 from 'parse5';
-declare var parse5Utils: any;
-var parse5Utils = require("parse5-utils")
 
 @Component({
 	selector: 'home',
 	styleUrls: ['./home.component.css'],
 	templateUrl: './home.component.html',
 	encapsulation: ViewEncapsulation.None,
-
 })
 export class HomeComponent {
 	constructor(private commentService: CommentService, private sanitizer: DomSanitizer, elementRef: ElementRef, renderer: Renderer) {    // Listen to click events in the component
@@ -23,7 +19,6 @@ export class HomeComponent {
 
 	@Input() content: any = "See Spot Run.<br /><p>I am not spot? Run Spot Run.</p>Spot is a dog. Spot can run.";
 	@Input() comments: any;
-	counter: number = 0;
 	selectedSentence: number;
 	editor: any;
 
@@ -33,53 +28,13 @@ export class HomeComponent {
 	}
 
 	setSentence(index) {
-		console.log('click');
 		this.selectedSentence = index;
 	}
 
 	attachComments() {
-		var myHtml = parse5.parse(this.content);
-		this.walkTheDOM(myHtml, (node) => this.setUpSentences(node));
-		var html = parse5.serialize(myHtml);
-		this.content = this.sanitizer.bypassSecurityTrustHtml(html);
 		this.comments = this.commentService.findAllComments();
 	}
 
-	setUpSentences(node) {
-		if (node.nodeName === "#text" && node.parentNode.nodeName !== "span") { // Is it a Text node?
-			if (node.value.length > 0) { // Does it have non white-space text content?
-				var sentences = node.value.replace(/([\.\?\!\n]\s*)/, "$&|||").split("|||");
-				var siblings = node.parentNode.childNodes;
-				var index = node.parentNode.childNodes.indexOf(node);
-
-				sentences.forEach(sentence => {
-					if (sentence.length) {
-						var newNode = parse5Utils.createNode("span");
-						var newTextNode = parse5Utils.createTextNode(sentence);
-						parse5Utils.append(newNode, newTextNode);
-						parse5Utils.setAttribute(newNode, "id", this.counter.toString());
-						parse5Utils.setAttribute(newNode, "class", "sentence");
-						siblings.splice(index, 0, newNode);
-						index++;
-						this.counter++;
-					}
-				});
-				parse5Utils.remove(node);
-			}
-		}
-	}
-
-	walkTheDOM(node, func) {
-		func(node);
-		var nodeIndex = 0;
-		node = node.childNodes && node.childNodes[nodeIndex];
-		while (node) {
-			this.walkTheDOM(node, func);
-			nodeIndex++;
-			node = node.parentNode && node.parentNode.childNodes && node.parentNode.childNodes[nodeIndex];
-		}
-		nodeIndex = 0;
-	}
 
 }
 
--- src/app/new-doc/new-doc.component.ts
+++ src/app/new-doc/new-doc.component.ts
@@ -1,18 +1,23 @@
-import { Component, OnDestroy, AfterViewInit, EventEmitter, Input, Output, NgZone } from '@angular/core';
+import { Component, OnDestroy, AfterViewInit, EventEmitter, Input, Output, NgZone, ViewEncapsulation } from '@angular/core';
 import { DomSanitizer } from '@angular/platform-browser';
+import * as parse5 from 'parse5';
+declare var parse5Utils: any;
 declare var tinymce: any;
-
+var parse5Utils = require("parse5-utils")
 tinymce = require('tinymce');
 
 @Component({
 	selector: 'new-doc',
 	styleUrls: ['./new-doc.component.css'],
-	templateUrl: './new-doc.component.html'
+	templateUrl: './new-doc.component.html',
+	encapsulation: ViewEncapsulation.None,
+
 })
 export class NewDocComponent {
 	constructor(private _ngZone: NgZone, private sanitizer: DomSanitizer) { }
 	@Input() elementId: String;
 	@Input() content: any;
+	counter: number = 0;
 
 	editor: any;
 
@@ -37,6 +42,70 @@ export class NewDocComponent {
 
 	ngOnDestroy() {
 		tinymce.remove(this.editor);
+	}
+
+	save() {
+		console.log('saving');
+		var myHtmlString = tinymce.activeEditor.getContent();
+		var myHtmlDoc = parse5.parse(myHtmlString);
+		console.log(this.content, myHtmlString, myHtmlDoc);
+		this.walkTheDOM(myHtmlDoc, (node) => this.setUpSentences(node));
+		var html = parse5.serialize(myHtmlDoc);
+		this.updateContent(html);
+	}
+
+	setUpSentences(node) {
+
+		//still broken. need to fix.
+		//strategy:
+		//parse entire doc to text.
+		//split into array by sentences using below regex.
+		//surround sentences with span tags.
+		//for each sentence, use another regex to check for any unclosed tags. close them, in order, before the 
+		//last span.
+		//check also for any unopened tags. open them, in order?
+
+		//what about this?
+		//some<p>sentence like this. sentence like <bold>this</bold>. final</p>sentence.
+		//ideal scenario is
+		//<span>some</span><p><span>sentence like this.</span><span>sentence like <bold>this</bold>.</span><span>final</span></p><span>sentence</span>
+
+		//GENIUS.com-- when you highlight text that spans multiple tags, it dives down to the textNode and wraps it with a genius tag, so that 
+		//formatting is not disrupted. it then applys the same ID to all the wrapped text nodes, so that when the user highlights the 'appearance'
+		//of continuity is maintained. i.e. if one ID is hovered activate the hover class on all elements with the same Id. smart.
+
+		if (node.nodeName === "#text" && node.parentNode.nodeName !== "span") { // Is it a Text node?
+			if (node.value.length > 0) { // Does it have non white-space text content?
+				var sentences = node.value.replace(/([\.\?\!\n]\s*)/g, "$&|||").split("|||");
+				var siblings = node.parentNode.childNodes;
+				var index = node.parentNode.childNodes.indexOf(node);
+				sentences.forEach(sentence => {
+					if (sentence.length) {
+						var newNode = parse5Utils.createNode("span");
+						var newTextNode = parse5Utils.createTextNode(sentence);
+						parse5Utils.append(newNode, newTextNode);
+						parse5Utils.setAttribute(newNode, "id", this.counter.toString());
+						parse5Utils.setAttribute(newNode, "class", "sentence");
+						siblings.splice(index, 0, newNode);
+						index++;
+						this.counter++;
+					}
+				});
+				parse5Utils.remove(node);
+			}
+		}
+	}
+
+	walkTheDOM(node, func) {
+		func(node);
+		var nodeIndex = 0;
+		node = node.childNodes && node.childNodes[nodeIndex];
+		while (node) {
+			this.walkTheDOM(node, func);
+			nodeIndex++;
+			node = node.parentNode && node.parentNode.childNodes && node.parentNode.childNodes[nodeIndex];
+		}
+		nodeIndex = 0;
 	}
 }
 
